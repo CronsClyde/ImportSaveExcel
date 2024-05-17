c6bb34814488f2df8e2b494db9b6fc97fbc32d7c
YangWooKim <cronsclyde@naver.com>
2024-05-17 17:02:39 +0900

    초기 커밋.

---
+++ ImportSaveExcel/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ImportSaveExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grdExcel = new DataGridView();
+            btnLoad = new Button();
+            txtExcelLoad = new RichTextBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)grdExcel).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grdExcel
+            // 
+            grdExcel.BackgroundColor = SystemColors.Window;
+            grdExcel.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grdExcel.Dock = DockStyle.Fill;
+            grdExcel.Location = new Point(0, 0);
+            grdExcel.Margin = new Padding(10);
+            grdExcel.Name = "grdExcel";
+            grdExcel.Size = new Size(800, 450);
+            grdExcel.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(663, 14);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 23);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "불러오기";
+            btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // txtExcelLoad
+            // 
+            txtExcelLoad.BackColor = SystemColors.Window;
+            txtExcelLoad.Location = new Point(25, 14);
+            txtExcelLoad.Name = "txtExcelLoad";
+            txtExcelLoad.Size = new Size(632, 23);
+            txtExcelLoad.TabIndex = 2;
+            txtExcelLoad.Text = "";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(txtExcelLoad);
+            panel1.Controls.Add(btnLoad);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 50);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(grdExcel);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 450);
+            panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)grdExcel).EndInit();
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView grdExcel;
+        private Button btnLoad;
+        private RichTextBox txtExcelLoad;
+        private Panel panel1;
+        private Panel panel2;
+    }
+}
---
+++ ImportSaveExcel/Form1.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Office.Interop.Excel;
+using ExcelDataReader;
+using System.Data;
+using System.Windows.Forms;
+using System.Text;
+
+namespace ImportSaveExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initEvent();
+        }
+
+        private void initEvent()
+        {
+            this.Load += form1_load;
+            btnLoad.Click += btnLoad_click;
+        }
+
+        private void form1_load(object sender, EventArgs e)
+        {
+            //grdExcel.RowSelectorVisibleStyle
+            //    rdOrd.RowSelectorVisibleStyle(RowSelectorStyle.RowIndex);
+            //grdOrd.DisplayLayout.Override.CellClickAction = CellClickAction.RowSelect;
+        }
+
+        private void btnLoad_click(object sender, EventArgs e)
+        {
+
+            Import_Excel(grdExcel);
+
+            ////File 경로
+            //string filepath = txtExcelLoad.Text;
+
+            ////APP 연결, WORKBOOK 설정, WORKSHEET 설정 
+            //Microsoft.Office.Interop.Excel.Application _app = new Microsoft.Office.Interop.Excel.Application();
+            //Microsoft.Office.Interop.Excel.Workbook _workbook = _app.Workbooks.Open(filepath, 0, true, 5, "", ":", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            ////첫 번째 SHEET 사용 / 만약 2번째 SHEET 사용 시 .get_Item(2) 
+            //Microsoft.Office.Interop.Excel.Worksheet _worksheet = (Microsoft.Office.Interop.Excel.Worksheet)_workbook.Worksheets.get_Item(1);
+            //object misvalue = System.Reflection.Missing.Value;
+
+            ////사용되어진 column의 갯수와 row 갯수를 변수로 저장 
+            //int column_Count = _worksheet.UsedRange.Columns.Count;
+            //int row_Count = _worksheet.UsedRange.Rows.Count;
+
+            //string acolumn = "";
+            //string totAcolumn = "";
+
+            ////1번째 줄부터, row_count까지 
+            //for (int i = 1; i <= row_Count; i++)
+            //{
+            //    if (_worksheet.get_Range("A" + i).Value2 == null)
+            //        acolumn = "";
+            //    else
+            //        acolumn = _worksheet.get_Range("A" + i).Value2.ToString();
+
+            //    totAcolumn += acolumn + "','";
+            //}
+        }
+
+
+        void Import_Excel(DataGridView grid, string fileName = "")
+        {
+            if (File.Exists(fileName) == false)
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel workbook|*.xlsx;*.xls", ValidateNames = true })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        fileName = ofd.FileName;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            FileInfo fi = new FileInfo(fileName);
+            if ((fi.Extension == ".xls" | fi.Extension == ".xlsx") == false)
+            {
+                return;
+            }
+            FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+            var enc = CodePagesEncodingProvider.Instance.GetEncoding(1252);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(fs);
+
+            DataSet result = reader.AsDataSet();
+            reader.Close();
+            if (result == null)
+            {
+                return;
+            }
+            System.Data.DataTable dt = result.Tables[0];
+
+            grid.DataSource = dt;
+            //foreach (DataRow row in dt.Rows)
+            //{
+            //    if (DBNull.Value.Equals(row.ItemArray[0]) == false)
+            //    {
+
+            //        grid.Rows.Add(row.ItemArray);
+            //    }
+            //}
+
+            string[] starry = new string[dt.Rows.Count];
+            int i = 0;
+            foreach (DataRow dr in dt.Rows) 
+            {
+                starry[i] = Convert.ToString(dr["Column2"]);
+                i++;
+            }
+        }
+
+       
+    }
+
+}
